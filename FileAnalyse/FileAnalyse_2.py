eee42988d72db6f6d900bad314569123aebbe1e2
yinchenjue <44486973+yinchenjue@users.noreply.github.com>
2019-05-06 21:48:24 +0800

    Create FileAnalyse_2.py

---
+++ FileAnalyse/FileAnalyse_2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import os
+import docReader
+import docWriter
+
+
+def main():
+    filetype = docReader.fileReader()
+    docreader = filetype.get_localizer("txt")
+    docwriter = docWriter.docWriter()
+
+    for childPath in os.listdir(docreader.getRootPathFunc()):
+        docreader.setAddressPathFunc(os.path.join(docreader.getRootPathFunc(), childPath))
+        docwriter.setTitle(childPath)
+        if docreader.openFileFunc():
+            if len(docreader.openFileFunc()) > 200:
+                docwriter.setFileContent(docreader.openFileFunc())
+                docwriter.docDeal()
+                docwriter.dataDeal()
+            else:
+                os.remove(os.path.join(docreader.getRootPathFunc(), childPath))
+
+main()
